83bfb67697a7b302df440a21e1998a9c0c11aa52
Feilx <1842068905@qq.com>
2018-09-18 16:32:25 +0800

    finish heap

--- Sorting/heap.c
+++ Sorting/heap.c
@@ -1,21 +1,72 @@
 #include<stdio.h>
-void quickSort(int*, int, int);
+void heapSort(int*, int);
+void heapAdjust(int*, int, int);
+void createHeap(int*, int);
 int main(void)
 {
-	int size = 50;
-	int num[50] = {23, 14, 35, 45, 12, 07, 32, 01, 98, 46,
+	int size = 51;
+	int num[51] = {00, 23, 14, 35, 45, 12, 07, 32, 01, 98, 46,
 		       87, 75, 54, 73, 92, 85, 62, 60, 63, 51,
 		       47, 49, 29, 39, 27, 61, 34, 58, 19, 21,
 		       86, 41, 05, 83, 69, 91,  8, 90, 99, 84,
 		       83, 58, 30, 10, 15, 82, 23, 28, 95, 94};
-	quickSort(num, 0, 49);
+	createHeap(num, 50);
+	heapSort(num, 50);
 	int count;
-	for(count = 0; count < size; count ++)
+	for(count = 1; count < size; count ++)
 	{
-		if(count % 10 == 0)
+		if(count % 10 == 1)
 			printf("\n");
 		printf("%d\t", num[count]);
 	}
 	printf("\n");
 	return 0;
+}
+
+void heapSort(int* num, int size)
+{
+	int temp;
+	for(temp = size; temp > 0; temp --)
+	{
+		int inter = num[size];
+		num[size] = num[1];
+		num[1] = inter;
+		size --;
+		heapAdjust(num, size, 1);
+	}
+}
+
+void heapAdjust(int* num, int size, int par)
+{
+	int temp, pos, lc, rc;
+	while(par <= size / 2)
+	{
+		temp = num[par];
+		lc = par << 1;
+		rc = lc + 1;
+		if(num[par] < num[lc])
+		{
+			num[par] = num[lc];
+			pos = lc;
+		}
+		if(rc <= size && num[par] < num[rc])
+		{
+			num[par] = num[rc];
+			pos = rc;
+		}
+		if(par == pos)
+			break;
+		else
+			num[pos] = temp;
+		par = pos;
+	}
+}
+
+void createHeap(int* num, int size)
+{
+	int i;
+	for(i = size / 2; i != 0; i--)
+	{
+		heapAdjust(num, size, i);
+	}
 }
