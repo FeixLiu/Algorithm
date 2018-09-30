6b1fce49422d99f335f7247c7bc67cf8bd8e4928
Feilx <1842068905@qq.com>
2018-09-30 17:10:05 +0800

    finish chapter 2

--- numerical_algorithm/all_code.c
+++ numerical_algorithm/all_code.c
@@ -243,8 +243,8 @@ bool isCarmicheal(int number)
 		if(GCD(number, i) == 1)
 		{
 			int result = fastModPower(i, number - 1, number); //Fermat little therome
-			if(result != 1) return false;
+			if(result != 1) return 0;
 		}
 	}
-	return true;
+	return 1;
 }
