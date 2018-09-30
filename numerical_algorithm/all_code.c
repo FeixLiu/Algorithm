7574a410f4aa9c2670c95e9b9f95cd33b41da11f
Feilx <1842068905@qq.com>
2018-09-30 15:55:43 +0800

    finish learning

--- numerical_algorithm/all_code.c
+++ numerical_algorithm/all_code.c
@@ -70,3 +70,86 @@ int is_prime(int p, int max_test)
 		if(pow(test, p - 1) % p != 1) return 0;
 	return 1; //still has a probability of pow(0.5, max_tests) that p is not a prime
 }
+
+float function(float); //function for all ingegral calculation
+//use rectangle to calculate integration
+float useRectangle(float xmin, float xmax, int num_interval)
+{
+	float dx = (xmax - xmin) / num_interval;
+	float total_area = 0;
+	float x = xmin;
+	int i;
+	for(i = 0; i < num_interval; i ++)
+	{
+		total_area += dx * function(x);
+		x += dx;
+	}
+	return total_area;
+}
+
+//use trapezoid to calculate integration
+float useTrapezoid(float xmin, float xmax, int num_interval)
+{
+	float dx = (xmax - xmin) / num_interval;
+	float total_area = 0;
+	float x = xmin;
+	int i;
+	for(i = 0; i < num_interval; i ++)
+	{
+		total_area += dx * (function(x) + function(x + dx)) / 2.0;
+		x += dx;
+	}
+	return total_area;
+}
+
+//use self-adaptation to calculate integration
+float useSelfAdaptation(float xmin, float xmax, int num_interval, float max_error)
+{
+	float dx = (xmax - xmin) / num_interval;
+	float total_area = 0;
+	float x = xmin;
+	int i;
+	for(i = 0; i < num_interval; i ++)
+	{
+		total_area += sliceArea(x, x + dx, max_error);
+		x += dx;
+	}
+	return total_area;
+}
+
+float sliceArea(float x1, float x2, float max_error)
+{
+	float y1 = function(x1);
+	float y2 = function(x2);
+	float xmid = (x1 + x2) / 2.0;
+	float ymid = function(xmid);
+
+	float area12 = (x2 - x1) * (y1 + y2) / 2.0;
+	float area1m = (xmid - x1) * (y1 + ymid) / 2.0;
+	float aream2 = (x2 - xmid) * (ymid + y2) / 2.0;
+	float area1m2 = area1m + aream2;
+
+	float error = (area1m2 - area12) / area12;
+
+	if(abs(error) < max_error)
+		return area1m2;
+	else
+		return sliceArea(x1, xmid, max_error) + sliceArea(xmid, x2, max_error);
+}
+
+float function(float); //function
+float dfdx(float); //function for calculating derivation
+//use Newton method to find one foot for function(x)
+float findZero(float initial_guess, float max_error)
+{
+	float x = initial_guess;
+	int i;
+	for(i = 0; i < 100; i ++) //we only calculate 100 times
+	{
+		float y = function(x);
+		if(abs(y) < max_error) 
+			break;
+		x -= y / dfdx(x); //use the derivation to update the value of x
+	}
+	return x;
+}
